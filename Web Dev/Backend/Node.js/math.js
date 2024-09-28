9f5e0050f227d3e5e6f895de19c2882f7cdca8fe
Abhay-Singh-75 <abhaysingh7533@gmail.com>
2024-09-28 15:17:55 +0530

    Node.js Learned

---
+++ Web Dev/Backend/Node.js/math.js
@@ -0,0 +1,27 @@
+// This Code in written by using the require()
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const g = 9.8;
+// const PI = 3.14;
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     Pi: PI,
+// }
+
+// module.exports = obj;
+
+
+
+
+
+
+
+// This code is written by using the import 
+
+export const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const PI= 3.14;
+export const g = 9.8;
---
+++ Web Dev/Backend/Node.js/script.js
@@ -0,0 +1,31 @@
+// let num = 5;
+// for(let i=0; i<num; i++){
+//     console.log("Hello World!");
+// }
+
+// console.log("Bye!");
+
+// console.log(process.argv);
+
+// let args = process.argv;
+// for(let i=2; i<args.length; i++){
+//     console.log(args[i]);
+// }
+
+// const math = require("./math");
+
+// console.log(math.sum(2,4));
+// console.log(math.g);
+
+
+// const file = require("./Fruits");
+// for(let i=0; i<file.length; i++){
+//     console.log(file[i].name);
+// }
+
+
+// linking the file by using the import
+
+import {sum, PI} from "./math.js"; 
+
+console.log(sum(2,3), PI);
