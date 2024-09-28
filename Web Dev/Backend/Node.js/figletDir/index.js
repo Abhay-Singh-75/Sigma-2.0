6cafa04d3273eda3276eb57b1575894828c9ae68
Abhay-Singh-75 <abhaysingh7533@gmail.com>
2024-09-28 23:05:12 +0530

    Using the figlet directory

--- Web Dev/Backend/Node.js/figletDir/index.js
+++ Web Dev/Backend/Node.js/figletDir/index.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet"); 
 
-figlet("Abhay Singh : )", function (err, data) {
+figlet("Raghav Singh", function (err, data) {
     if (err) {
       console.log("Something went wrong...");
       console.dir(err);
