f2c234a23f29f07cc58fd33620cfe51afd639d76
Abhay-Singh-75 <abhaysingh7533@gmail.com>
2025-02-17 16:52:54 +0530

    Revising the concept

--- Web Dev/Backend/MongoDB with Express/index.js
+++ Web Dev/Backend/MongoDB with Express/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");   
@@ -19,7 +20,7 @@ main()
     })
     .catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsaap');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsaap');
 }
 
 app.get("/", (req, res) => {
@@ -31,11 +32,17 @@ app.listen(8080, () => {
 });
 // Ends Here  
 
+
 // Index Route
-app.get("/chats", async (req,res) => {
-    let chats = await Chat.find();
-    // console.log(chats);
-    res.render("index.ejs", { chats });
+app.get("/chats", async (req,res,next) => {
+    try{
+        let chats = await Chat.find();
+        // console.log(chats);
+        res.render("index.ejs", { chats });
+    }   catch(err){
+        next();
+    }
+    
 });
 
 // New Route for inserting data on database
@@ -44,41 +51,75 @@ app.get("/chats/new", (req,res) => {
 })
 
 // Create Route OR Inserting data in DB through Page 
-app.post("/chats", (req,res) => {
-    let {from, to, message } = req.body;
-    let newChat = new Chat({
-        from: from,
-        to: to,
-        message: message,
-        created_at: new Date(),
-    });
-    res.redirect("/chats");
-
-    newChat.save()
-    .then((res) => {
-        console.log(res);
-        console.log("Inserted Successfully");
-    }).catch((err) => {
-        console.log(err);
-    })
+app.post("/chats", async (req,res,err) => {
+    try{
+        let {from, to, message } = req.body;
+        let newChat = new Chat({
+            from: from,
+            to: to,
+            message: message,
+            created_at: new Date(),
+        });
+        res.redirect("/chats");
+    
+        await newChat.save()
+        .then((res) => {
+            console.log(res);
+            console.log("Inserted Successfully");
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    catch(err){
+        next(err);
+    }
+    
 });
+
 
 
 // This is Edit route -> in this we edit the chats 
-app.get("/chats/:id/edit", async (req,res) => {
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs", {chat}); 
+app.get("/chats/:id/edit", async (req,res,next) => {
+
+    try{
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        res.render("edit.ejs", {chat}); 
+    }
+    catch(err){
+        next();
+    }
+    
 });
 
+// Show route
+app.get("/chats/:id", asyncWrap(async (req,res,next) => {
+
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+    
+        if(!chat){
+            next(new ExpressError(404, "Chat not found"));
+        }
+        else{
+            res.render("edit.ejs", {chat});
+        }
+}));
+
 //  This update route 
-app.put("/chats/:id", async (req,res) => {
-    let {id} = req.params;
-    let {message: newMessage} = req.body; //Isme humne {message: newMessage} isley kraa hsi kyuki hum uska naam "message" se badl ke "newMessage" kr rhe haii 
-    let updateChat = await Chat.findByIdAndUpdate(id, {message: newMessage}, {runValidator: true, new: true });
-    //  isme hamne new true isley kra hai kyuki hume terminal new wali chat print krani haii
-    console.log(updateChat);
-    res.redirect("/chats");
+app.put("/chats/:id", async (req,res,next) => {
+
+    try{
+        let {id} = req.params;
+        let {message: newMessage} = req.body; //Isme humne {message: newMessage} isley kraa hsi kyuki hum uska naam "message" se badl ke "newMessage" kr rhe haii 
+        let updateChat = await Chat.findByIdAndUpdate(id, {message: newMessage}, {runValidator: true, new: true });
+        //  isme hamne new true isley kra hai kyuki hume terminal new wali chat print krani haii
+        console.log(updateChat);
+        res.redirect("/chats");
+    }   catch(err){
+        next(err);
+    }
+    
 });
 
 // Destroy Route or we Delete chats route
@@ -90,3 +131,22 @@ app.delete("/chats/:id", async (req,res) => {
     res.redirect("/chats"); 
     console.log("deleted Successfully");
 });
+
+app.use((err,req,res,next) =>{
+    console.log(err.name);
+    next();
+});
+
+
+app.use((err,req,res,next) => {
+    let {status = 500, message= "Some error occured"} = err;
+    res.status(status).send(message);
+})
+
+
+
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err) => next(err));
+    }
+}
